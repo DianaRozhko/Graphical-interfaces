3d21922ab01a3aecf8315846480a83f10b8c3cbd
DianaRozhko <rozhko.diana06@gmail.com>
2025-03-24 21:28:59 +0200

    lab3: adding project and solution

---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace FortuneTeller
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string[] answers = new string[]
+        {
+            "Так", "Ні", "Скоріше так", "Скоріше ні", "Можливо", "Запитай пізніше","Це залежить лише від тебе"
+        };
+
+        private readonly Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AskFortune_Click(object sender, RoutedEventArgs e)
+        {
+            string question = QuestionTextBox.Text.Trim();
+            if (string.IsNullOrEmpty(question))
+            {
+                AnswerText.Text = "Будь ласка, введи запитання!";
+                return;
+            }
+
+            int index = random.Next(answers.Length);
+            AnswerText.Text = $"На твоє питання: \"{question}\"\nВідповідь: {answers[index]}";
+        }
+    }
+}
